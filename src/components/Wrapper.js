edbedd88a5a3399e5494248bbca107a9560f7586
andersonms1 <andersoonn.ams@gmail.com>
2020-09-28 13:32:13 -0300

    :art: Create Wrapper component

---
+++ src/components/Wrapper.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useStyletron } from "baseui";
+
+const Wrapper = ({ children }) => {
+  const [css, theme] = useStyletron();
+  // const { offset, color, children, forwardedRef, type } = props;
+
+  return (
+    <div
+      className={css({
+        position: "absolute",
+        // top: offset || "46%",
+        // left: offset || "46%",
+        // width: '200px',
+        // paddingBottom: '2px',
+        // paddingRight: "20px",
+        // textAlign: 'center',
+        // backgroundColor: color,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+
+const WrapperLoayalty = ({ children }) => {
+  const [css, theme] = useStyletron();
+  // const { offset, color, children, forwardedRef, type } = props;
+
+  return (
+    <div
+      className={css({
+        position: "absolute",
+        // top: offset || "46%",
+        // left: offset || "46%",
+        // width: '200px',
+        // paddingBottom: '2px',
+        // paddingRight: "20px",
+        // textAlign: 'center',
+        // backgroundColor: color,
+        paddingTop: `${theme.sizing.scale500}`,
+        paddingLeft: `${theme.sizing.scale750}`,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { Wrapper, WrapperLoayalty };
